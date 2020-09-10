3860027310ac362a7e4efc8eae87e4ced10d2d36
JulianZhengId <zheng_julian@yahoo.com>
2020-09-10 12:05:41 +0200

    Initial Commit

---
+++ Assets/CostumChanger.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CostumChanger : MonoBehaviour
+{
+    public Material[] faceMaterials;
+    public Material[] bodyMaterials;
+    public Mesh[] hornMeshes;
+
+    public GameObject playerAvatar;
+
+    private SkinnedMeshRenderer faceSkinnedMR;
+    private SkinnedMeshRenderer bodySkinnedMR;
+    private SkinnedMeshRenderer hornSkinnedMR;
+
+    public int indexFaceMaterial = -1;
+    public int indexBodyMaterial = -1;
+    public int indexHornMesh = -1;
+
+    private void Start()
+    {
+        faceSkinnedMR = playerAvatar.transform.Find("Face").GetComponentInChildren<SkinnedMeshRenderer>();
+        bodySkinnedMR = playerAvatar.transform.Find("Dragon_body").GetComponentInChildren<SkinnedMeshRenderer>();
+        hornSkinnedMR = playerAvatar.transform.Find("Horn").GetComponentInChildren<SkinnedMeshRenderer>();
+        indexFaceMaterial = GetMaterialIndex(faceMaterials, faceSkinnedMR);
+        indexBodyMaterial = GetMaterialIndex(bodyMaterials, bodySkinnedMR);
+        indexHornMesh = GetMeshIndex(hornMeshes);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Change Face");
+            if (indexFaceMaterial >= faceMaterials.Length - 1)
+            {
+                indexFaceMaterial = 0;
+                faceSkinnedMR.material = faceMaterials[indexFaceMaterial];
+            }
+            else
+            {
+                faceSkinnedMR.material = faceMaterials[indexFaceMaterial + 1];
+                indexFaceMaterial++;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Change Body");
+            if (indexBodyMaterial >= bodyMaterials.Length - 1)
+            {
+                indexBodyMaterial = 0;
+                bodySkinnedMR.material = bodyMaterials[indexBodyMaterial];
+            }
+            else
+            {
+                bodySkinnedMR.material = bodyMaterials[indexBodyMaterial + 1];
+                indexBodyMaterial++;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Change Horn");
+            if (indexHornMesh >= hornMeshes.Length - 1)
+            {
+                indexHornMesh = 0;
+                hornSkinnedMR.sharedMesh = hornMeshes[indexHornMesh];
+            }
+            else
+            {
+                hornSkinnedMR.sharedMesh = hornMeshes[indexHornMesh + 1];
+                indexHornMesh++;
+            }
+        }
+    }
+
+    private int GetMaterialIndex(Material[] myMaterials, SkinnedMeshRenderer mySMR)
+    {
+        int index = -1;
+        for (int i = 0; i < faceMaterials.Length; i++)
+        {
+            if (myMaterials[i].name + " (Instance)" == GetCurrentFaceMaterial(mySMR).name || myMaterials[i].name == GetCurrentFaceMaterial(mySMR).name)
+            {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    private int GetMeshIndex(Mesh[] myMeshes)
+    {
+        int index = -1;
+        for (int i = 0; i < hornMeshes.Length; i++)
+        {
+            if (hornMeshes[i].name + " (Instance)" == GetCurrentHornMesh().name || hornMeshes[i].name == GetCurrentHornMesh().name)
+            {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    private Material GetCurrentFaceMaterial(SkinnedMeshRenderer mySMR)
+    {
+        return mySMR.materials[0];
+    }
+
+    private Mesh GetCurrentHornMesh()
+    {
+        return hornSkinnedMR.sharedMesh;
+    }
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject UndestroyedPlayer;
+    public static GameController GC;
+    public GameObject photonPlayer;
+    public GameObject playerSet;
+
+    public void OnClickLoadSene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void Awake()
+    {
+        Debug.Log("Awake");
+        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(UndestroyedPlayer);
+    }
+
+    // called first
+    void OnEnable()
+    {
+        Debug.Log("OnEnable called");
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    // called second
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 1)
+        {
+            playerSet = Instantiate(photonPlayer, new Vector3(0, 0, 0), Quaternion.identity);
+            UndestroyedPlayer.transform.parent = playerSet.transform;
+            
+        }
+        Debug.Log("OnSceneLoaded: " + scene.name);
+        Debug.Log(mode);
+    }
+
+    // called third
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // called when the game is terminated
+    void OnDisable()
+    {
+        Debug.Log("OnDisable");
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    public GameObject GetPlayerSet()
+    {
+        return playerSet;
+    }
+}
